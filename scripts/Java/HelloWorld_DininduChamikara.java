64ffdc6df1b827ef0501104ebcd9a1d7b10b9756
DininduChamikara <dininduchamikara99@gmail.com>
2023-10-04 20:22:03 +0530

    completed: hello world program in java

---
+++ scripts/Java/HelloWorld_DininduChamikara.java
@@ -0,0 +1,16 @@
+// LANGUAGE: Java
+// ENV: JRE
+// AUTHOR: Dinindu Chamikara
+// GITHUB: https://github.com/DininduChamikara
+
+package scripts.Java;
+
+class HelloWorld_DininduChamikara {
+    public final void sayHelloWorld() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String args[]) {
+        new HelloWorld_DininduChamikara().sayHelloWorld();
+    }
+}
